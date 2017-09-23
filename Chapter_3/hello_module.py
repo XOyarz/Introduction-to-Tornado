eeb15a8aa4d20d5d8826182f1fd441ddc0ff3fb1
Xavier <xavier982@hotmail.com>
2017-09-23 08:52:33 +0200

    UI Modules

---
+++ Chapter_3/hello_module.py
@@ -0,0 +1,53 @@
+import tornado,web
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import os.path
+
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
+
+class HelloHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('hello.html')
+
+class HelloModule(tornado.web.UIModule):
+    def render(self):
+        return '<h1>Hello, world!</h1>'
+
+class BookModule(tornado.web.UIModule):
+    def render(self, book):
+        return self.render_string('modules/book.html', book=book)
+
+class RecommendedHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render(
+            "recommended.html",
+            page_title="Burt's Books | Recommended Reading",
+            header_text="Recommended Reading",
+            book=[
+                {
+                    "title": "Programming Collective Intelligence",
+                    "subtitle": "Building Smart Web 2.0 Applications",
+                    "image": "/static/images/collective_intelligence.gif",
+                    "author": "Toby Segaran",
+                    "date_added": 1310248056,
+                    "date_released": "August 2007",
+                    "isbn": "978-0-596-52932-1",
+                    "description": "<p>This fascinating book demonstrates how you ...</p>"
+
+                },
+            ]
+        )
+
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(
+        handlers=[(r'/', HelloHandler)],
+        template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+        ui_modules={'Hello', HelloModule}
+    )
+    server = tornado.httpserver.HTTPServer(app)
+    server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
